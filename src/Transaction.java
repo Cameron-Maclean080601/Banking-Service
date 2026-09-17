df9ec443787aa26aa7690db10d5a4d7df1a8b27b
Cameron Maclean <Camimac@hotmail.co.uk>
2026-09-17 21:04:21 +0100

    Javadoc Comments

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,8 +1,16 @@
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+/**
+ * A class used to create a simple transaction
+ *
+ * <p>A transaction has an ID, date, description, category, amount,
+ * and currency. IDs and amounts must be positive, and a description
+ * must be supplied.
+ * <p/>
+ * Currency and category are backed by enums to ensure sensible inputs.
+ */
 public class Transaction{
-
     Integer id;
     LocalDate date;
     String description;
@@ -10,10 +18,30 @@ public class Transaction{
     BigDecimal amount;
     Currency currency;
 
+
+    /**
+     * Creates a new transaction.
+     *
+     * @param id          the unique positive ID of the transaction
+     * @param date        the date of the transaction
+     * @param description the description of the transaction
+     * @param category    the category of the transaction
+     * @param amount      the positive transaction amount
+     * @param currency    the currency of the transaction
+     */
     public Transaction (Integer id, LocalDate date, String description, Category category, BigDecimal amount, Currency currency)
     {
+
         if (id <= 0){
             throw new IllegalArgumentException("ID must be positive");
+        }
+
+        if (description.isEmpty()){
+            throw new IllegalArgumentException("Transaction must contain description");
+        }
+
+        if (amount.compareTo(BigDecimal.ZERO) > 0){
+            throw new IllegalArgumentException("Amount must be positive");
         }
 
         this.id = id;
